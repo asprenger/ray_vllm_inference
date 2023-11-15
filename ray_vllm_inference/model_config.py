45b1570e40832539eb1689e0c063c00ef6832b13
Andre Sprenger <sprenger@andresprenger.com>
2023-11-15 10:50:44 +0100

    Refactor setup.py

--- ray_vllm_inference/model_config.py
+++ ray_vllm_inference/model_config.py
@@ -6,6 +6,9 @@ def load_model_config(model_id:str, config_path:Optional[str]=None) -> ModelConf
     if not config_path:
         dir_path = os.path.dirname(os.path.realpath(__file__))
         config_path = os.path.join(dir_path, 'models')
+        if not os.path.isdir(config_path):
+            config_path = os.path.join(os.path.dirname(dir_path), 'models')
+    print(config_path)
     path = os.path.join(config_path, model_id.replace('/', '--')) + '.yaml'
     with open(path, "r") as stream:
         return ModelConfig.parse_yaml(stream)
--- setup.py
+++ setup.py
@@ -29,31 +29,6 @@ def get_version() -> str:
     version = find_version(get_path("ray_vllm_inference", "__init__.py"))
     return version
 
-
-common_setup_kwargs = {
-    "name": "ray_vllm_inference",
-    "version": "0.1.0",
-    "description": "A service that integrates vLLM with Ray Serve for fast and scalable LLM serving.",
-    "author": "Andre Sprenger",
-    "license": "Apache 2.0",
-    "license_files": "LICENSE.txt",
-    "python_requires": ">=3.8.0",
-    "long_description": read_readme(),
-    "long_description_content_type": "text/markdown",
-    "url": "https://github.com/asprenger/ray_vllm_inference",
-    "keywords": ["python", "vllm", "ray", "llm"],
-    "platforms": ["linux"],
-    "classifiers": [
-        "Environment :: GPU :: NVIDIA CUDA :: 11.8",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: Apache Software License",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence"
-    ]
-}
-
 requirements = [
     "ray==2.8.0",
     "ray[serve]==2.8.0",
@@ -63,8 +38,29 @@ requirements = [
 ]
 
 setup(
-    packages=find_packages(),
+    name="ray_vllm_inference",
+    version="0.1.0",
+    description="A service that integrates vLLM with Ray Serve for fast and scalable LLM serving.",
+    author="Andre Sprenger",
+    license="Apache 2.0",
+    license_files="LICENSE.txt",
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/asprenger/ray_vllm_inference",
+    keywords=["python", "vllm", "ray", "llm"],
+    platforms= ["linux"],
+    classifiers=[
+        "Environment :: GPU :: NVIDIA CUDA :: 11.8",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    packages=find_packages(include=['ray_vllm_inference', 'ray_vllm_inference.*']),
     install_requires=requirements,
     include_package_data=True,
-    **common_setup_kwargs
+    package_data={"ray_vllm_inference": ["models/*"]},
+    python_requires=">=3.8.0",
 )
