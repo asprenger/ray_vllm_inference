d67a0e62211661efb86ce1e03beb2038b51eb3a3
Andre Sprenger <sprenger@andresprenger.com>
2023-11-15 11:27:49 +0100

    Cleanup

--- ray_vllm_inference/model_config.py
+++ ray_vllm_inference/model_config.py
@@ -8,7 +8,6 @@ def load_model_config(model_id:str, config_path:Optional[str]=None) -> ModelConf
         config_path = os.path.join(dir_path, 'models')
         if not os.path.isdir(config_path):
             config_path = os.path.join(os.path.dirname(dir_path), 'models')
-    print(config_path)
     path = os.path.join(config_path, model_id.replace('/', '--')) + '.yaml'
     with open(path, "r") as stream:
         return ModelConfig.parse_yaml(stream)
